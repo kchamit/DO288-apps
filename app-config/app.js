08cc0e8c37d0a16389352677d6c53dc9f1de6106
Student User <student@workstation.lab.example.com>
2020-08-27 13:05:35 +0000

    update2

--- app-config/app.js
+++ app-config/app.js
@@ -13,7 +13,7 @@ app.get('/', function (req, res) {
     response = 'Value in the APP_MSG env var is => ' + msg + '\n';
     // read config
     const config = yaml.safeLoad(fs.readFileSync('/opt/app-root/config/config.yaml', 'utf8'));
-    response += 'The config file is => ' + config + '\n';
+    response += 'The config file is => ' + JSON.stringify(config) + '\n';
 
     // Read in the secret file
     fs.readFile('/opt/app-root/secure/myapp.sec', 'utf8', function (secerr,secdata) {
